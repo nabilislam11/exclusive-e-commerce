5a3092c67728b5b38b41db0ea5b554d0614c1093
Nabil Islam Hamza <hamjanail11@gmail.com>
2025-09-22 13:21:45 +0600

    flashsale

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,12 +6,13 @@ import Thismonth from '../components/thismonth/Thismonth'
 import Advertisement from '../components/advertisement/Advertisement'
 import LocalProduct from '../components/localproduct/LocalProduct'
 import Featured from '../components/featured/Featured'
+import Flashsale from '../components/flashsale/flashSale'
 
 const Home = () => {
   return (
     <div>
       <Banner></Banner>
-      <FlashSale></FlashSale>
+      <Flashsale></Flashsale>
       <Categoris></Categoris>
       <Thismonth></Thismonth>
       <Advertisement></Advertisement>
