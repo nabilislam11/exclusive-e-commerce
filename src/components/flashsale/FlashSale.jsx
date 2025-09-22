cb9c74a4008d1fb5fd7f222e73cc811e75046f19
Nabil Islam Hamza <hamjanail11@gmail.com>
2025-09-22 13:16:37 +0600

    trying bug fiexed

--- src/components/flashsale/FlashSale.jsx
+++ src/components/flashsale/FlashSale.jsx
@@ -15,210 +15,206 @@ import ProductCard from '../local/ProductCard';
 
 
 const RatingStars = ({ rating }) => {
-  return (
-    <div className="flex items-center">
-      {Array.from({ length: 5 }).map((_, i) => {
-        const full = i + 1 <= Math.floor(rating);   // full star
-        const half = i < rating && i + 1 > rating;  // half star
+    return (
+        <div className="flex items-center">
+            {Array.from({ length: 5 }).map((_, i) => {
+                const full = i + 1 <= Math.floor(rating);   // full star
+                const half = i < rating && i + 1 > rating;  // half star
 
-        return (
-          <span key={i}>
-            {full ? (
-              <FaStar className="text-yellow-500" size={14} />
-            ) : half ? (
-              <FaStarHalfAlt className="text-yellow-500" size={14} />
-            ) : (
-              <FaRegStar className="text-gray-300" size={14} />
-            )}
-          </span>
-        );
-      })}
-    </div>
-  );
+                return (
+                    <span key={i}>
+                        {full ? (
+                            <FaStar className="text-yellow-500" size={14} />
+                        ) : half ? (
+                            <FaStarHalfAlt className="text-yellow-500" size={14} />
+                        ) : (
+                            <FaRegStar className="text-gray-300" size={14} />
+                        )}
+                    </span>
+                );
+            })}
+        </div>
+    );
 };
 
 
 const today = [
-  {
-    id: 1,
-    img: todays1,
-    tittle: "HAVIT HV- G92 Gamepad",
-    oldPrice: ` $190`,
-    newPrice: `$120`,
-    discount: "-40%",
-    ratingStar: 2.5,
-    rating: 88,
-  },
-  {
-    id: 2,
-    img: todays2,
-    tittle: "AK-900 Wired Keyboard",
-    oldPrice: `$960`,
-    newPrice: `$1160`,
-    discount: "-40%",
-    ratingStar: 5,
-    rating: 75,
-  },
-  {
-    id: 3,
-    img: todays3,
-    tittle: "IPS LCD Gaming Monitor",
-    oldPrice: `$370`,
-    newPrice: `$400`,
-    discount: "-40%",
-    ratingStar: 3,
-    rating: 99,
-  },
-  {
-    id: 4,
-    img: todays4,
-    tittle: "S-Series Comfort Chair ",
-    oldPrice: `$375`,
-    newPrice: `$400`,
-    discount: "-40%",
-    ratingStar: 4,
-    rating: 98,
-  },
-  {
-    id: 5,
-    img: todays3,
-    tittle: "HAVIT HV- G92 Gamepad",
-    oldPrice: 190,
-    newPrice: 120,
-    discount: "-40%",
-    ratingStar: 5,
-    rating: 88,
-  },
-  {
-    id: 1,
-    img: todays3,
-    tittle: "HAVIT HV- G92 Gamepad",
-    oldPrice: 190,
-    newPrice: 120,
-    discount: "-40%",
-    ratingStar: 4.5,
-    rating: 88,
-  },
+    {
+        id: 1,
+        img: todays1,
+        tittle: "HAVIT HV- G92 Gamepad",
+        oldPrice: ` $190`,
+        newPrice: `$120`,
+        discount: "-40%",
+        ratingStar: 2.5,
+        rating: 88,
+    },
+    {
+        id: 2,
+        img: todays2,
+        tittle: "AK-900 Wired Keyboard",
+        oldPrice: `$960`,
+        newPrice: `$1160`,
+        discount: "-40%",
+        ratingStar: 5,
+        rating: 75,
+    },
+    {
+        id: 3,
+        img: todays3,
+        tittle: "IPS LCD Gaming Monitor",
+        oldPrice: `$370`,
+        newPrice: `$400`,
+        discount: "-40%",
+        ratingStar: 3,
+        rating: 99,
+    },
+    {
+        id: 4,
+        img: todays4,
+        tittle: "S-Series Comfort Chair ",
+        oldPrice: `$375`,
+        newPrice: `$400`,
+        discount: "-40%",
+        ratingStar: 4,
+        rating: 98,
+    },
+    {
+        id: 5,
+        img: todays3,
+        tittle: "HAVIT HV- G92 Gamepad",
+        oldPrice: 190,
+        newPrice: 120,
+        discount: "-40%",
+        ratingStar: 5,
+        rating: 88,
+    },
+    {
+        id: 1,
+        img: todays3,
+        tittle: "HAVIT HV- G92 Gamepad",
+        oldPrice: 190,
+        newPrice: 120,
+        discount: "-40%",
+        ratingStar: 4.5,
+        rating: 88,
+    },
 ]
 
 const NextArrow = ({ onClick }) => {
-  return (
-    <button
-      onClick={onClick}
-      className="absolute right-[350px] top-[-55px] transform -translate-y-1/2 bg-gray-200  text-black py-[10px] px-[10px] rounded-full shadow-md hover:bg-gray-300  "
-    >
-      <MdArrowForwardIos size={20} />
-    </button>
-  );
+    return (
+        <button
+            onClick={onClick}
+            className="absolute right-[350px] top-[-55px] transform -translate-y-1/2 bg-gray-200  text-black py-[10px] px-[10px] rounded-full shadow-md hover:bg-gray-300  "
+        >
+            <MdArrowForwardIos size={20} />
+        </button>
+    );
 };
 
 const PrevArrow = ({ onClick }) => {
-  return (
-    <button
-      onClick={onClick}
-      className="absolute right-[410px] top-[-55px] transform -translate-y-1/2 bg-gray-200  text-black py-[10px] px-[10px] rounded-full shadow-md hover:bg-gray-300 "
-    >
-      <MdArrowBackIos size={20} />
-    </button>
-  );
+    return (
+        <button
+            onClick={onClick}
+            className="absolute right-[410px] top-[-55px] transform -translate-y-1/2 bg-gray-200  text-black py-[10px] px-[10px] rounded-full shadow-md hover:bg-gray-300 "
+        >
+            <MdArrowBackIos size={20} />
+        </button>
+    );
 };
 
 const FlashSale = () => {
-  var settings = {
-    dots: false,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 5,
-    slidesToScroll: 1,
-    arrows: true,
-    autoplay: false,
-    nextArrow: <NextArrow />,   // custom next
-    prevArrow: <PrevArrow />,   // custom prev
-    responsive: [
-      {
-        breakpoint: 1024,
-        settings: { slidesToShow: 3, slidesToScroll: 1 }
-      },
-      {
-        breakpoint: 600,
-        settings: { slidesToShow: 2, slidesToScroll: 1 }
-      },
-      {
-        breakpoint: 480,
-        settings: { slidesToShow: 1, slidesToScroll: 1 }
-      }
-    ]
-  };
-  return (
-    <div className='border-b-1 border-gray-400  '>
-      <Container>
-        {/* todays__part */}
-        <div class="relative inline-block group h-[30px]  overflow-hidden rounded-md">
-
-          <div class="absolute top-0 left-0 h-full w-3 bg-red-500 transition-all duration-500 group-hover:w-full"></div>
-
-
-          <span class="relative z-10 px-4 py-2 font-semibold text-red-500 transition-colors duration-500 group-hover:text-white">
-            Today’s
-          </span>
-        </div>
-        {/* Flesh sales__part */}
-        <div className="flex gap-[87px] items-center pt-[24px] pb-[40px] ">
-          <h1 className='font-primary font-semibold text-[36px] leading-[48px]'>Flash Sales</h1>
-          <div className="flex items-center gap-x-[17px] ">
-            <div className=" flex flex-col gap-y-[4px] ">
-              <h1 className='font-secondary font-medium  text-[12px] leading-[18px]'>Days</h1>
-              <p className='font-primary font-bold text-[32px] leading-[30px] '>03</p>
-
-            </div>
-            <p className='text-[#E07575] w-[4px] h-[16px] '>:</p>
-            <div className=" flex flex-col gap-y-[4px]">
-              <h1 className='font-secondary font-medium  text-[12px] leading-[18px]'>Hours</h1>
-              <p className='font-primary font-bold text-[32px] leading-[30px] '>23</p>
-            </div>
-            <p className='text-[#E07575] w-[4px] h-[16px] '>:</p>
-            <div className="flex flex-col gap-y-[4px]">
-              <h1 className='font-secondary font-medium  text-[12px] leading-[18px]'>Minutes</h1>
-              <p className='font-primary font-bold text-[32px] leading-[30px] '>19</p>
-
-            </div>
-            <p className='text-[#E07575] w-[4px] h-[16px] '>:</p>
-            <div className="flex flex-col gap-y-[4px]">
-              <h1 className='font-secondary font-medium  text-[12px] leading-[18px]'>Seconds</h1>
-              <p className='font-primary font-bold text-[32px] leading-[30px] '>50</p>
-
-            </div>
-          </div>
-
-
-
-
-
-        </div>
-        {/* product_card */}
-
-        <div className="slider-container 
-        ">
-          <Slider {...settings}>
+    var settings = {
+        dots: false,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 5,
+        slidesToScroll: 1,
+        arrows: true,
+        autoplay: false,
+        nextArrow: <NextArrow />,   // custom next
+        prevArrow: <PrevArrow />,   // custom prev
+        responsive: [
             {
-              today.map((items) => (
-                <ProductCard items={items}></ProductCard>
-              ))
+                breakpoint: 1024,
+                settings: { slidesToShow: 3, slidesToScroll: 1 }
+            },
+            {
+                breakpoint: 600,
+                settings: { slidesToShow: 2, slidesToScroll: 1 }
+            },
+            {
+                breakpoint: 480,
+                settings: { slidesToShow: 1, slidesToScroll: 1 }
             }
+        ]
+    };
+    return (
+        <div className='border-b-1 border-gray-400  '>
+            <Container>
+                {/* todays__part */}
+                <div class="relative inline-block group h-[30px]  overflow-hidden rounded-md">
+
+                    <div class="absolute top-0 left-0 h-full w-3 bg-red-500 transition-all duration-500 group-hover:w-full"></div>
+
+
+                    <span class="relative z-10 px-4 py-2 font-semibold text-red-500 transition-colors duration-500 group-hover:text-white">
+                        Today’s
+                    </span>
+                </div>
+                {/* Flesh sales__part */}
+                <div className="flex gap-[87px] items-center pt-[24px] pb-[40px] ">
+                    <h1 className='font-primary font-semibold text-[36px] leading-[48px]'>Flash Sales</h1>
+                    <div className="flex items-center gap-x-[17px] ">
+                        <div className=" flex flex-col gap-y-[4px] ">
+                            <h1 className='font-secondary font-medium  text-[12px] leading-[18px]'>Days</h1>
+                            <p className='font-primary font-bold text-[32px] leading-[30px] '>03</p>
+
+                        </div>
+                        <p className='text-[#E07575] w-[4px] h-[16px] '>:</p>
+                        <div className=" flex flex-col gap-y-[4px]">
+                            <h1 className='font-secondary font-medium  text-[12px] leading-[18px]'>Hours</h1>
+                            <p className='font-primary font-bold text-[32px] leading-[30px] '>23</p>
+                        </div>
+                        <p className='text-[#E07575] w-[4px] h-[16px] '>:</p>
+                        <div className="flex flex-col gap-y-[4px]">
+                            <h1 className='font-secondary font-medium  text-[12px] leading-[18px]'>Minutes</h1>
+                            <p className='font-primary font-bold text-[32px] leading-[30px] '>19</p>
+
+                        </div>
+                        <p className='text-[#E07575] w-[4px] h-[16px] '>:</p>
+                        <div className="flex flex-col gap-y-[4px]">
+                            <h1 className='font-secondary font-medium  text-[12px] leading-[18px]'>Seconds</h1>
+                            <p className='font-primary font-bold text-[32px] leading-[30px] '>50</p>
+
+                        </div>
+                    </div>
+
+                </div>
+                {/* product_card */}
+
+                <div className="slider-container 
+        ">
+                    <Slider {...settings}>
+                        {
+                            today.map((items) => (
+                                <ProductCard items={items}></ProductCard>
+                            ))
+                        }
 
 
 
 
-          </Slider>
-          <div className="flex items-center justify-center pt-[51px] pb-[60px]  ">
-            <button className=' py-[16px] px-[48px] bg-red-500 text-white font-secondary font-medium  text-[16px] leading-[24px] rounded '>View All Products</button>
-          </div>
+                    </Slider>
+                    <div className="flex items-center justify-center pt-[51px] pb-[60px]  ">
+                        <button className=' py-[16px] px-[48px] bg-red-500 text-white font-secondary font-medium  text-[16px] leading-[24px] rounded '>View All Products</button>
+                    </div>
+                </div>
+
+
+            </Container>
         </div>
-
-
-      </Container>
-    </div>
-  )
+    )
 }
 
 
