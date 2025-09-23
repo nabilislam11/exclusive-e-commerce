65c7a65bccb2eee4ab9204938b73f140e35e16af
Nabil Islam Hamza <hamjanail11@gmail.com>
2025-09-24 00:46:38 +0600

    emmeni

--- src/components/service/Service.jsx
+++ src/components/service/Service.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import services1 from '../../assets/services1.png'
+
 import services2 from '../../assets/services2.png'
 import services3 from '../../assets/services3.png'
 import Container from '../container/Container'
@@ -9,7 +9,7 @@ const Service = () => {
             <Container>
                 <div className=' flex justify-around items-center pb-[140px] '>
                     <div className="items-center ">
-                        <img className=' pb-[24px] mx-auto ' src={services1} alt="" />
+                        {/* <img className=' pb-[24px] mx-auto ' src={services1} alt="" /> */}
                         <div className="flex flex-col gap-y-2 ">
                             <h4 className='font-secondary  font-semibold text-[20px] leading-[28px]'>FREE AND FAST DELIVERY</h4>
                             <p className='font-secondary  font-normal text-[14px] leading-[21px]'>Free delivery for all orders over $140</p>
