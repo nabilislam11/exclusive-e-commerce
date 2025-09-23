3ddbd16951b126bdeca9efa361a11d7364a634d5
Nabil Islam Hamza <hamjanail11@gmail.com>
2025-09-24 00:52:34 +0600

    emnei 2

--- src/components/service/Service.jsx
+++ src/components/service/Service.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
 
-import services2 from '../../assets/services2.png'
-import services3 from '../../assets/services3.png'
+import Services1 from '../../assets/Services1.png'
+import Services2 from '../../assets/Services2.png'
+import Services3 from '../../assets/Services3.png'
 import Container from '../container/Container'
 const Service = () => {
     return (
@@ -9,21 +10,21 @@ const Service = () => {
             <Container>
                 <div className=' flex justify-around items-center pb-[140px] '>
                     <div className="items-center ">
-                        {/* <img className=' pb-[24px] mx-auto ' src={services1} alt="" /> */}
+                        <img className=' pb-[24px] mx-auto ' src={Services1} alt="" />
                         <div className="flex flex-col gap-y-2 ">
                             <h4 className='font-secondary  font-semibold text-[20px] leading-[28px]'>FREE AND FAST DELIVERY</h4>
                             <p className='font-secondary  font-normal text-[14px] leading-[21px]'>Free delivery for all orders over $140</p>
                         </div>
                     </div>
                     <div className=" items-center  ">
-                        <img className=' pb-[24px] mx-auto ' src={services2} alt="" />
+                        <img className=' pb-[24px] mx-auto ' src={Services2} alt="" />
                         <div className="flex flex-col gap-y-2 ">
                             <h4 className='font-secondary  font-semibold text-[20px] leading-[28px]'>FREE AND FAST DELIVERY</h4>
                             <p className='font-secondary  font-normal text-[14px] leading-[21px]'>Free delivery for all orders over $140</p>
                         </div>
                     </div>
                     <div className=" items-center  ">
-                        <img className=' pb-[24px]  mx-auto' src={services3} alt="" />
+                        <img className=' pb-[24px]  mx-auto' src={Services3} alt="" />
                         <div className="flex flex-col gap-y-2 ">
                             <h4 className='font-secondary  font-semibold text-[20px] leading-[28px]'>FREE AND FAST DELIVERY</h4>
                             <p className='font-secondary  font-normal text-[14px] leading-[21px]'>Free delivery for all orders over $140</p>
