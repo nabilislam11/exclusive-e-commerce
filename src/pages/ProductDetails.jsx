5afb9d428059b55371b0f512c617ecf8083cea04
Nabil Islam Hamza <hamjanail11@gmail.com>
2025-12-01 15:23:40 +0600

    redux isshu fixed

--- src/pages/ProductDetails.jsx
+++ src/pages/ProductDetails.jsx
@@ -33,6 +33,7 @@ const ProductDetails = () => {
         }
     }
     const handleBuy = () => {
+        console.log("buying", product)
         dispatch(cartDetails(product))
 
 
--- src/store.js
+++ src/store.js
@@ -1,7 +1,7 @@
 import { configureStore } from "@reduxjs/toolkit";
-import cartReducer from "./slice/cartSlice";
+import cartSlice from "./slice/cartSlice";
 export default configureStore({
   reducer: {
-    cartinfo: cartReducer,
+    cartinfo: cartSlice,
   },
 });
